b9305a89074c85db3e34e58f05cb0f31c8507b55
Eretic <Eretic@users.noreply.github.com>
2015-12-13 20:54:19 +0200

    Start version

---
+++ crawler/JJDecoder.py
@@ -0,0 +1,274 @@
+import re
+
+
+OUTPUT = ''
+
+
+def out(s):
+    global OUTPUT
+    OUTPUT += s
+
+
+def lotu(gv, data):
+    # lotu
+    str_l = "(![]+\"\")[" + gv + "._$_]+"
+    str_o = gv + "._$+"
+    str_t = gv + ".__+"
+    str_u = gv + "._+"
+
+    if data.find(str_l) == 0:
+        data = data[len(str_l):]
+        return data, 'l'
+    elif data.find(str_o) == 0:
+        data = data[len(str_o):]
+        return data, 'o'
+    elif data.find(str_t) == 0:
+        data = data[len(str_t):]
+        return data, 't'
+    elif data.find(str_u) == 0:
+        data = data[len(str_u):]
+        return data, 'u'
+    return data, ''
+
+
+def decode(text):
+    text = re.sub(r'/^\s+|\s+$/g', '', text)
+
+    if text.find("\"\'\\\"+\'+\",") == 0:
+        # locate jjcode
+        start_pos = text.find('$$+"\\""+') + 8;
+        end_pos = text.find('"\\"")())()');
+        # get gv
+        gv = text[text.find('"\'\\"+\'+",') + 9:text.find("=~[]")]
+        gvl = len(gv)
+    else:
+        # get gv
+        gv = text[:text.find('=')]
+        gvl = len(gv)
+
+        # locate jjcode
+        start_pos = text.find('"\\""+') + 5
+        end_pos = text.find('"\\"")())()')
+
+    if start_pos == end_pos:
+        print('No data')
+        return ''
+
+    data = text[start_pos:end_pos]
+    # hex decode string
+    b = ["___+", "__$+", "_$_+", "_$$+",
+         "$__+", "$_$+", "$$_+", "$$$+",
+         "$___+", "$__$+", "$_$_+", "$_$$+",
+         "$$__+", "$$_$+", "$$$_+", "$$$$+"]
+
+    # # lotu
+    # str_l = "(![]+\"\")[" + gv + "._$_]+"
+    # str_o = gv + "._$+"
+    # str_t = gv + ".__+"
+    # str_u = gv + "._+"
+
+    # 0123456789abcdef
+    str_hex = gv + "."
+
+    # s
+    str_s = '"'
+    gvsig = gv + "."
+
+    str_quote = '\\\\\\"'
+    str_slash = '\\\\\\\\'
+
+    str_lower = "\\\\\"+"
+    str_upper = "\\\\\"+" + gv + "._+"
+
+    str_end = '"+'  # end of s loop
+
+    while data:
+        # lotu
+        data, lc = lotu(gv, data)
+        if lc:
+            out(lc)
+            continue
+
+        # 0123456789abcdef
+        if data.find(str_hex) == 0:
+            data = data[len(str_hex):]
+            # check every element of hex decode string for a match
+            for i in range(len(b)):
+                if data.find(b[i]) == 0:
+                    data = data[len(b[i]):]
+                    out('%x' % i)
+                    break
+            continue
+
+        # start of s block
+        if data.startswith(str_s):
+            data = data[len(str_s):]
+            # check if "R
+            if data.startswith(str_upper):  # r4 n >= 128
+                data = data[len(str_upper):]  # skip sig
+                ch_str = []
+                for j in range(2):  # shouldn't be more than 2 hex chars
+                    # gv + "."+b[ c ]
+                    if data.startswith(gvsig):
+                        data = data[len(gvsig):]  # skip gvsig
+                        for k in range(len(b)):  # for every entry in b
+                            if data.startswith(b[k]):
+                                data = data[len(b[k])]
+                                ch_str.append(k)
+                                break
+                    else:
+                        break  # done
+                out(''.join([chr(x) for x in ch_str]))
+                continue
+            elif data.startswith(str_lower):
+                data = data[len(str_lower):]  # skip sig
+                ch_str = ""
+                ch_lotux = ""
+                temp = ""
+                b_checkR1 = 0
+
+                for j in range(3):  # shouldn't be more than 3 octal chars
+                    if j > 1:  # lotu check
+                        data, ch_lotux = lotu(gv, data)
+                        if ch_lotux:
+                            break
+
+                    # gv + "."+b[ c ]
+                    if data.startswith(gvsig):
+                        temp = data[len(gvsig):]
+                        for k in range(8):  # for every entry in b octal
+                            if temp.startswith(b[k]):
+                                if int(ch_str + str(k), 8) > 128:
+                                    b_checkR1 = 1
+                                    break
+                                ch_str += str(k)
+                                data = data[len(gvsig):]  # skip gvsig
+                                data = data[len(b[k]):]
+                                break
+
+                        if b_checkR1 == 1:
+                            if data.startswith(str_hex):
+                                data = data[len(str_hex):]
+                                # check every element of hex decode string for a match
+                                for i in range(len(b)):
+                                    if data.find(b[i]) == 0:
+                                        data = data[len(b[i]):]
+                                        ch_lotux = hex(i)
+                                        break
+                                break
+                    else:
+                        break
+                out(chr(int(ch_str, 8)) + ch_lotux)
+                continue  # step out of the while loop
+            else:  # "S ----> "SR or "S+
+                # if there is, loop s until R 0r +
+                # if there is no matching s block, throw error
+
+                match = 0
+                n = 0
+                # searching for mathcing pure s block
+                while True:
+                    n = ord(data[0])
+                    if data.startswith(str_quote):
+                        data = data[len(str_quote):]
+                        out('"')
+                        match += 1
+                        continue
+                    elif data.startswith(str_slash):
+                        data = data[len(str_slash):]
+                        out('\\')
+                        match += 1
+                        continue
+                    elif data.startswith(str_end):
+                        if match == 0:
+                            print('+ no match S block: ' + data)
+                            return
+                        data = data[len(str_end):]
+                        break  # step out of the while loop
+                    elif data.startswith(str_upper):
+                        if match == 0:
+                            print('no match S block n>128: ' + data)
+                            return
+                        data = data[len(str_upper):]
+                        ch_str = []
+                        for j in range(10):
+                            if j > 1:
+                                data, ch_lotux = lotu(gv, data)
+                                if ch_lotux:
+                                    break
+                            # gv + "."+b[ c ]
+                            if data.startswith(gvsig):
+                                data = data[len(gvsig):]
+                                for k in range(len(b)):
+                                    if data.startswith(b[k]):
+                                        data = data[len(b[k]):]
+                                        ch_str.append(k)
+                                        break
+                            else:
+                                break  # done
+                        out(''.join([chr(x) for x in ch_str]))
+                        break  # step out of the while loop
+                    elif data.startswith(str_lower):
+                        if match == 0:
+                            print('no match S block n<128: ' + data)
+                            return
+                        data = data[len(str_lower):]
+                        ch_str = ''
+                        b_checkR1 = 0
+                        for j in range(3):
+                            if j > 1:
+                                data, ch_lotux = lotu(gv, data)
+                                if ch_lotux:
+                                    break
+                            # gv + "."+b[ c ]
+                            if data.startswith(gvsig):
+                                temp = data[len(gvsig):]
+                                for k in range(8):
+                                    if temp.startswith(b[k]):
+                                        if int(ch_str + str(k), 8) > 128:
+                                            b_checkR1 = 1
+                                            break
+                                        ch_str += str(k);
+                                        data = data[len(gvsig):]  # skip gvsig
+                                        data = data[len(b[k]):];
+                                        break
+                                if b_checkR1 == 1:
+                                    if data.startswith(str_hex):
+                                        data = data[len(str_hex):]
+                                        for i in range(len(b)):
+                                            if data.startswith(b[i]):
+                                                data = data[len(b[i]):]
+                                                ch_lotux = hex(i)
+                                                break
+                            else:
+                                break  # done
+                        out(chr(int(ch_str, 8)) + ch_lotux)
+                        break  # step out of the while loop
+                    elif (0x21 <= n <= 0x2f) or (0x3A <= n <= 0x40) or (0x5b <= n <= 0x60) or (0x7b <= n <= 0x7f):
+                        out(data[0])
+                        data = data[1:]
+                        match += 1
+                continue
+        print('No match: ' + data)
+        break
+
+    global OUTPUT
+    return OUTPUT
+
+
+def test():
+    with open('jjdecoder_tests.txt', 'r') as tests:
+        req = tests.readline()
+        exp = tests.readline()
+        result = decode(req)
+        if result == exp:
+            print('Test passed')
+        else:
+            print('Input: ' + req)
+            print('Exp: ' + exp)
+            print('Result: ' + result)
+            print('Test FAILED')
+
+
+if __name__ == '__main__':
+    test()
---
+++ crawler/UzCrawler.py
@@ -0,0 +1,230 @@
+import JJDecoder
+
+import time
+import json
+import pickle
+import pprint
+import requests
+
+
+UZ_BASE = 'http://booking.uz.gov.ua/'
+
+
+class UzTownCode:
+    def __init__(self, path=None):
+        self.database = dict()
+        if not path:
+            path = 'uz_station_codes.txt'
+        self.path = path
+        with open(path, 'rb') as f:
+            self.database = pickle.load(f)
+
+    def get(self, station):
+        return self.database.get(station, '')
+
+    def put(self, station, station_id):
+        if station not in self.database:
+            self.database[station] = station_id
+            self.save()
+
+    def put_all(self, data):
+        self.database.update(data)
+        self.save()
+
+    def save(self):
+        print('Save database')
+        with open(self.path, 'wb') as f:
+            pickle.dump(self.database, f)
+
+
+class Point:
+    def __init__(self, data=None):
+        if not data:
+            data = dict()
+        self.date = data.get('date', 0)
+        self.src_date = data.get('src_date', '')
+        self.station = data.get('station', '')
+        self.station_id = data.get('station_id', '')
+        code = UzTownCode()
+        code.put(self.station, self.station_id)
+
+    def __repr__(self):
+        return '<Point where=%s when=%s>' % (self.station, self.date)
+
+
+class Train:
+    def __init__(self, data=None):
+        if not data:
+            data = dict()
+        self.category = data.get('category', -1)
+        self.model = data.get('model', -1)
+        self.num = data.get('num', '')
+        self.source = Point(data.get('from', dict()))
+        self.destination = Point(data.get('till', dict()))
+        self.coaches = data.get('types', list())
+
+    def __repr__(self):
+        return '<Train num=%s\n\tfrom=%s\n\ttill=%s>' % (self.num, self.source, self.destination)
+
+
+class UzCrawler:
+    def __init__(self):
+        self.user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
+        self.cookies = None
+        self.token = ''
+        self.server = ''
+        self.session_id = ''
+
+    def get(self, page):
+        headers = dict()
+        headers['User-Agent'] = self.user_agent
+        resp = requests.get(page, headers=headers, cookies=self.cookies)
+        self.cookies = resp.cookies
+        raw_token = resp.content.decode('utf-8')
+        marker_start = "_gaq.push(['_trackPageview']);"
+        marker_stop = "(function ()"
+        t_start = raw_token.find(marker_start)
+        t_stop = raw_token.find(marker_stop, t_start)
+        if t_start != -1 and t_stop != -1:
+            raw_token = raw_token[t_start + len(marker_start):t_stop]
+            print('TOKEN: ' + raw_token)
+            decoded_token = JJDecoder.decode(raw_token)
+            print(decoded_token)
+            self.token = decoded_token[decoded_token.find('", "') + 4:-3]
+            print(self.token)
+        for c in self.cookies:
+            if c.name == 'HTTPSERVERID':
+                self.server = c.value
+            elif c.name == '_gv_sessid':
+                self.session_id = c.value
+        return resp
+
+    def dump_cookies(self):
+        if not self.cookies:
+            print('Cookies is not found')
+            return
+        for c in self.cookies:
+            pprint.pprint(c)
+
+    def uz_post(self, url, post_data):
+        add_headers = dict()
+        add_headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*;q=0.8'
+        add_headers['Accept-Language'] = 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,bg;q=0.2'
+        add_headers['GV-Token'] = self.token
+        add_headers['GV-Unique-Host'] = 1
+        add_headers['GV-Ajax'] = 1
+        add_headers['GV-Screen'] = '1920x1080'
+        add_headers['GV-Referer'] = 'http://booking.uz.gov.ua/ru/'
+        add_headers['Referer'] = 'http://booking.uz.gov.ua/ru/'
+        add_headers['Host'] = 'booking.uz.gov.ua'
+        add_headers['Proxy-Connection'] = 'keep-alive'
+        add_headers['Origin'] = 'http://booking.uz.gov.ua'
+        #add_headers['Cookie'] = '_gv_sessid=%s; _gv_lang=ru; HTTPSERVERID=%s; __utmt=1; ' \
+        #                        '__utma=31515437.2137620103.1434828433.1434828433.1434828433.1; ' \
+        #                        '__utmb=31515437.2.10.1434828433; __utmc=31515437; ' \
+        #                        '__utmz=31515437.1434828433.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)' % (self.session_id, self.server)
+        add_headers['User-Agent'] = self.user_agent
+        resp = requests.post(UZ_BASE + url, data=post_data,
+                             cookies=self.cookies,
+                             headers=add_headers)
+        print('uz_post %s - %d' % (url, resp.status_code))
+        return resp
+
+    def search(self, from_city, to_city, when):
+        url = 'ru/purchase/search/'
+        coder = UzTownCode()
+        from_city_id = coder.get(from_city)
+        to_city_id = coder.get(to_city)
+
+        if not from_city_id:
+            print('I do not know id for ' + from_city)
+            return None
+        if not to_city_id:
+            print('I do not know id for ' + to_city)
+            return None
+
+        data = dict()
+        data['station_id_from'] = from_city_id
+        data['station_id_till'] = to_city_id
+        data['station_from'] = from_city
+        data['station_till'] = to_city
+        data['date_dep'] = when
+        data['time_dep'] = '00:00'
+        data['time_dep_till'] = ''
+        data['another_ec'] = 0
+        data['search'] = ''
+
+        resp = self.uz_post(url, data)
+        trains = json.loads(resp.content.decode('utf-8'))
+        if not trains['error']:
+            trains = trains['value']
+        else:
+            print('Error in search')
+            pprint.pprint(trains)
+            return None
+        #self.cookies = resp.cookies
+        trains = [Train(x) for x in trains]
+
+        return trains
+
+    def coaches(self, train='724K', coach_type='C2'):
+        url = 'ru/purchase/coaches/'
+        data = dict()
+        data['station_id_from'] = 2200001
+        data['station_id_till'] = 2204001
+        data['train'] = train
+        data['coach_type'] = coach_type
+        data['model'] = 1
+        data['date_dep'] = 1451129700
+        data['another_ec'] = 0
+        data['round_trip'] = 0
+
+        resp = self.uz_post(url, data)
+        if not resp.ok:
+            print(resp.status_code)
+            return None
+        coaches = json.loads(resp.content.decode('utf-8'))
+        pprint.pprint(coaches)
+        return resp
+
+    def coach(self, train='724K', coach_type='C2'):
+        url = 'ru/purchase/coaches/'
+        data = dict()
+        data['station_id_from'] = 2200001
+        data['station_id_till'] = 2204001
+        data['train'] = train
+        data['coach_type'] = coach_type
+        data['model'] = 1
+        data['date_dep'] = 1451129700
+        data['another_ec'] = 0
+        data['round_trip'] = 0
+
+        resp = self.uz_post(url, data)
+        if not resp.ok:
+            print(resp.status_code)
+            return None
+        coaches = json.loads(resp.content.decode('utf-8'))
+        pprint.pprint(coaches)
+        return resp
+
+
+def get_start_page():
+    crawler = UzCrawler()
+    resp = crawler.get(UZ_BASE)
+    for h in resp.headers:
+        pprint.pprint(h)
+    pprint.pprint(resp.cookies)
+    pprint.pprint(resp.content)
+    crawler.dump_cookies()
+    print('Search trains')
+    trains = crawler.search('Киев-Пассажирский', 'Харьков-Пасс', '24.12.2015')
+    pprint.pprint(trains)
+    crawler.dump_cookies()
+    time.sleep(5)
+    for x in range(5):
+        crawler.coaches()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    print('Test UzCrawler')
+    get_start_page()
